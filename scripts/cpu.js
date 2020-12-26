e7d1918142f9c2e9583c0c2781805e562a53062d
Juan Rafael Garcia Blanco <juanrgar@localhost.localdomain>
2020-12-26 14:53:26 +0100

    Implement more instructions

--- scripts/cpu.js
+++ scripts/cpu.js
@@ -6,7 +6,6 @@ class Cpu {
         this.memory = new Array(4096);
         this.v = new Array(16);
         this.i = 0;
-        this.vf = 0;
         this.delayTimer = 0;
         this.soundTimer = 0;
         this.pc = 0;
@@ -15,19 +14,19 @@ class Cpu {
 
         this.decode_level1 = [
             this.decode_level1_0,
-            this.decode_level1_1,
-            this.decode_level1_2,
-            this.decode_level1_3,
-            this.decode_level1_4,
-            this.decode_level1_5,
-            this.decode_level1_6,
-            this.decode_level1_7,
+            this.inst_JP,
+            this.inst_CALL,
+            this.inst_SE_imm,
+            this.inst_SNE_imm,
+            this.inst_SE_reg,
+            this.inst_LD_imm,
+            this.inst_ADD_imm,
             this.decode_level1_8,
-            this.decode_level1_9,
-            this.decode_level1_A,
-            this.decode_level1_B,
-            this.decode_level1_C,
-            this.decode_level1_D,
+            this.inst_SNE_reg,
+            this.inst_LD_I,
+            this.inst_JP_V0,
+            this.inst_RND,
+            this.inst_DRW,
             this.decode_level1_E,
             this.decode_level1_F
         ];
@@ -86,54 +85,18 @@ class Cpu {
         }
     }
 
-    decode_level1_1(inst) {
-    }
-
-    decode_level1_2(inst) {
-    }
-
-    decode_level1_3(inst) {
-    }
-
-    decode_level1_4(inst) {
-    }
-
-    decode_level1_5(inst) {
-    }
-
-    decode_level1_6(inst) {
-    }
-
-    decode_level1_7(inst) {
-    }
-
     decode_level1_8(inst) {
-    }
-
-    decode_level1_9(inst) {
-    }
-
-    decode_level1_A(inst) {
-    }
-
-    decode_level1_B(inst) {
-    }
-
-    decode_level1_C(inst) {
-    }
-
-    decode_level1_D(inst) {
     }
 
     decode_level1_E(inst) {
     }
 
-    decode_level1_(inst) {
+    decode_level1_F(inst) {
     }
 
     inst_SYS(inst) {
-        let addr = inst & 0xFFFFFF;
-        this.set_pc(addr);
+        let nnn = inst & 0xFFFFFF;
+        this.set_pc(nnn);
     }
 
     inst_CLS(inst) {
@@ -144,6 +107,200 @@ class Cpu {
     inst_RET(inst) {
         this.set_pc(this.stack.pop());
         this.dec_sp();
+    }
+
+    inst_JP(inst) {
+        let nnn = inst & 0xFFFFFF;
+        this.set_pc(nnn);
+    }
+
+    inst_JP_V0(inst) {
+        let nnn = inst & 0xFFFFFF;
+        this.set_pc(nnn + this.v[0]);
+    }
+
+    inst_CALL(inst) {
+        let nnn = inst & 0xFFFFFF;
+        this.inc_sp();
+        this.stack.push(this.pc);
+        this.set_pc(nnn);
+    }
+
+    inst_SE_imm(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let kk = inst & 0xFFFF;
+        let inc = 1 + (this.v[x] == kk);
+        this.inc_pc(inc);
+    }
+
+    inst_SNE_imm(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let kk = inst & 0xFFFF;
+        let inc = 1 + (this.v[x] != kk);
+        this.inc_pc(inc);
+    }
+
+    inst_SE_reg(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let y = (inst >> 8) & 0xFF;
+        let inc = 1 + (this.v[x] == this.v[y]);
+        this.inc_pc(inc);
+    }
+
+    inst_SNE_reg(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let y = (inst >> 8) & 0xFF;
+        let inc = 1 + (this.v[x] != this.v[y]);
+        this.inc_pc(inc);
+    }
+
+    inst_LD_imm(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let kk = inst & 0xFFFF;
+        this.v[x] = kk;
+    }
+
+    inst_LD_reg(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let y = (inst >> 8) & 0xFF;
+        this.v[x] = this.v[y];
+    }
+
+    inst_LD_I(inst) {
+        let nnn = inst & 0xFFFFFF;
+        this.i = nnn;
+    }
+
+    inst_LD_DT_rd(inst) {
+        let x = (inst >> 16) & 0xFF;
+        this.v[x] = this.delayTimer;
+    }
+
+    inst_LD_DT_wr(inst) {
+        let x = (inst >> 16) & 0xFF;
+        this.delayTimer = this.v[x];
+    }
+
+    inst_LD_ST_wr(inst) {
+        let x = (inst >> 16) & 0xFF;
+        this.soundTimer = this.v[x];
+    }
+
+    inst_LD_F(inst) {
+        console.log('not implemented');
+    }
+
+    inst_LD_B(inst) {
+        console.log('not implemented');
+    }
+
+    inst_LD_regs_st(inst) {
+        console.log('not implemented');
+    }
+
+    inst_LD_regs_ld(inst) {
+        console.log('not implemented');
+    }
+
+    inst_ADD_imm(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let kk = inst & 0xFFFF;
+        this.v[x] += kk;
+        this.v[x] &= 0xFF;
+    }
+
+    inst_ADD_reg(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let y = (inst >> 8) & 0xFF;
+        this.v[x] += this.v[y];
+        this.v[0xF] = this.v[x] > 0x100;
+        this.v[x] &= 0xFF;
+    }
+
+    inst_ADD_I(inst) {
+        let x = (inst >> 16) & 0xFF;
+        this.i += this.v[x];
+        this.i &= 0xFFFF;
+    }
+
+    inst_SUB(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let y = (inst >> 8) & 0xFF;
+        this.v[0xF] = this.v[x] > this.v[y];
+        this.v[x] -= this.v[y];
+        this.v[x] &= 0xFF;
+    }
+
+    inst_SUBN(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let y = (inst >> 8) & 0xFF;
+        this.v[0xF] = this.v[y] > this.v[x];
+        this.v[x] = this.v[y] - this.v[x];
+    }
+
+    inst_AND(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let y = (inst >> 8) & 0xFF;
+        this.v[x] &= this.v[y];
+    }
+
+    inst_OR(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let y = (inst >> 8) & 0xFF;
+        this.v[x] |= this.v[y];
+    }
+
+    inst_XOR(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let y = (inst >> 8) & 0xFF;
+        this.v[x] ^= this.v[y];
+    }
+
+    inst_SHR(inst) {
+        let x = (inst >> 16) & 0xFF;
+        this.v[0xF] = this.v[x] & 0x01;
+        this.v[x] >>= 1;
+    }
+
+    inst_SHL(inst) {
+        let x = (inst >> 16) & 0xFF;
+        this.v[0xF] = this.v[x] & 0x80;
+        this.v[x] <<= 1;
+        this.v[x] &= 0xFF;
+    }
+
+    inst_RND(inst) {
+        console.log('not implemented');
+    }
+
+    inst_DRW(inst) {
+        let x = (inst >> 16) & 0xFF;
+        let y = (inst >> 8) & 0xFF;
+        let n = inst & 0xFF;
+        let vx = this.v[x];
+        let vy = this.v[y];
+        this.v[0xF] = 0;
+        for (let r = 0; r < n; r++) {
+            let sc = this.memory[this.i + r];
+            for (let c = 0; c < 8; c++) {
+                if (sc & 0x80) {
+                    this.v[0xF] |= screen.setPixel(vx + c, vy + r);
+                }
+                sc <<= 1;
+            }
+        }
+    }
+
+    inst_SKP(inst) {
+        console.log('not implemented');
+    }
+
+    inst_SKNP(inst) {
+        console.log('not implemented');
+    }
+
+    inst_LD_K(inst) {
+        console.log('not implemented');
     }
 }
 
