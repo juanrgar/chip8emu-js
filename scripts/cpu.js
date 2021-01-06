93fba916cde64f4ac8fefa8407344522f3b0cac6
Juan Rafael Garcia Blanco <juanrgar@gmail.com>
2021-01-06 23:44:59 +0100

    Add copyright notice

--- scripts/cpu.js
+++ scripts/cpu.js
@@ -1,3 +1,22 @@
+/**
+ * This file is part of chip8emu-js.
+ *
+ * Copyright (C) 2020 Juan R. García Blanco <juanrgar@gmail.com>
+ *
+ * chip8emu-js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * chip8emu-js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with chip8emu-js.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 class Cpu {
     constructor(screen, keyboard) {
         this.screen = screen;
